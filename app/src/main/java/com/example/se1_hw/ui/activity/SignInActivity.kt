098afe4a1c975a785b4e5df53ea76905b6f6947e
cyj2825 <cyj2825@naver.com>
2021-06-09 20:56:17 +0900

    seventh assignment

--- app/src/main/java/com/example/se1_hw/ui/activity/SignInActivity.kt
+++ app/src/main/java/com/example/se1_hw/ui/activity/SignInActivity.kt
@@ -4,81 +4,75 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.se1_hw.api.ServiceCreator
+import com.example.se1_hw.data.SoptUserAuthStorage
+import com.example.se1_hw.data.local.SoptUserInfo
 import com.example.se1_hw.data.request.RequestLoginData
 import com.example.se1_hw.data.response.ResponseLoginData
 import com.example.se1_hw.databinding.ActivityMainBinding
+import com.example.se1_hw.utils.enqueueUtil
+import com.example.se1_hw.utils.showToast
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
 
+    private val signUpActivityLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        binding.idedit.setText(it.data?.getStringExtra("ID"))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        Log.d("onCreate", "onCreate(run)")
         setContentView(binding.root)
-        initButtonClickEvent()
-        initTextClickEvent()
+        searchUserAuthStorage()
+        loginButtonEvent()
+        signUpResult()
     }
-    private fun initButtonClickEvent() {
+    private fun searchUserAuthStorage() {
+        with(SoptUserAuthStorage.getInstance(this)) {
+            if (hasUserData()) {
+                requestLogin(getUserData().let { RequestLoginData(it.id, it.password) })
+            }
+        }
+    }
+
+    private fun loginButtonEvent() {
         binding.button.setOnClickListener{
             val requestLoginData = RequestLoginData(
                     id = binding.idedit.text.toString(),
                     password = binding.passedit.text.toString()
             )
-            val call: Call<ResponseLoginData> = ServiceCreator.soptService.postLogin(requestLoginData)
-            call.enqueue(object : Callback<ResponseLoginData> {
-                override fun onResponse(
-                        call: Call<ResponseLoginData>,
-                        response: Response<ResponseLoginData>
-                ){
-                    if(response.isSuccessful){
-                        val data = response.body()?.data
-                        Toast.makeText(this@SignInActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
-                        startHomeActivity()
-                    }else {
-                        Toast.makeText(
-                                this@SignInActivity,
-                                "아이디/비밀번호를 확인해주세요!",
-                                Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-                override fun onFailure(call: Call<ResponseLoginData>, t:Throwable){
-                    Log.d("NetworkTest", "error:$t")
-                }
-            })
-
+            requestLogin(requestLoginData)
+        }
+    }
+    private fun signUpResult() {
+        binding.signup.setOnClickListener {
+            signUpActivityLauncher.launch(
+                Intent(this, SignUpActivity::class.java)
+            )
         }
     }
     private fun startHomeActivity(){
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
     }
-    private fun initTextClickEvent(){
-        binding.signup.setOnClickListener{
-            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
-            startActivityForResult(intent, REQUEST_CODE)
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_CODE){
-            if(resultCode == Activity.RESULT_OK){
-                binding.idedit.setText(data!!.getStringExtra("id"))
-                binding.passedit.setText(data!!.getStringExtra("password"))
+
+    private fun requestLogin(requestLoginData: RequestLoginData) {
+        val call: Call<ResponseLoginData> = ServiceCreator.soptService
+            .postLogin(requestLoginData)
+        call.enqueueUtil(
+            onSuccess = { response ->
+                val data = response.data
+                showToast(data?.user_nickname.toString())
+                with(SoptUserAuthStorage.getInstance(this)) {
+                    saveUserData(requestLoginData.let { SoptUserInfo(it.id, it.password) })
+                }
+                startHomeActivity()
             }
-        }
+        )
     }
-
-    companion object{
-        private const val REQUEST_CODE = 100
-    }
-
-
 }
